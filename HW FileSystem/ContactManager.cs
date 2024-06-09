c0666a8ca716ab6a5d1482b81ac65d305f1617c8
Дмитрий Рындаков <branzino@mail.ru>
2024-06-09 22:20:41 +0500

    Доделал вторую часть

---
+++ HW FileSystem/ContactManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace HW_FileSystem
+{
+    public class ContactManager
+    {
+        private List<Contact> contacts = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void RemoveContact(string name)
+        {
+            var contactToRemove = contacts.FirstOrDefault(c => c.Name == name);
+            if (contactToRemove != null)
+            {
+                contacts.Remove(contactToRemove);
+            }
+            else
+            {
+                Console.WriteLine("Contact not found.");
+            }
+        }
+
+        public Contact FindContactByName(string name)
+        {
+            return contacts.FirstOrDefault(c => c.Name == name);
+        }
+
+        public Contact FindContactByPhoneNumber(string phoneNumber)
+        {
+            return contacts.FirstOrDefault(c => c.PhoneNumber == phoneNumber);
+        }
+
+        public void SaveToJson(string filePath)
+        {
+            try
+            {
+                var jsonString = JsonConvert.SerializeObject(contacts);
+                File.WriteAllText(filePath, jsonString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while saving to JSON: {ex.Message}");
+            }
+        }
+
+        public void SaveToXml(string filePath)
+        {
+            try
+            {
+                var xmlSerializer = new XmlSerializer(typeof(List<Contact>));
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    xmlSerializer.Serialize(stream, contacts);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while saving to XML: {ex.Message}");
+            }
+        }
+
+        public void LoadFromJson(string filePath)
+        {
+            try
+            {
+                var jsonString = File.ReadAllText(filePath);
+                contacts = JsonConvert.DeserializeObject<List<Contact>>(jsonString);
+            }
+                
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while loading from JSON: {ex.Message}");
+            }
+
+
+                
+               
+        }
+
+        public void LoadFromXml(string filePath)
+        {
+            try
+            {
+                var xmlSerializer = new XmlSerializer(typeof(List<Contact>));
+                using (var stream = new FileStream(filePath, FileMode.Open))
+                {
+                    contacts = (List<Contact>)xmlSerializer.Deserialize(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while loading from XML: {ex.Message}");
+            }
+        }
+
+        public void DisplayContacts()
+        {
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"Name: {contact.Name}, Phone Number: {contact.PhoneNumber}");
+            }
+        }
+    }
+}
--- HW FileSystem/Program.cs
+++ HW FileSystem/Program.cs
@@ -40,6 +40,86 @@
                 Console.WriteLine($"Name: {i.Name}, YearOfRealise:{i.YearOfRealise},Director: {i.Director}, Genre: {i.Genre}");
             }
             Console.WriteLine();
+
+            ContactManager manager = new ContactManager();
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Remove Contact");
+                Console.WriteLine("3. Find Contact by Name");
+                Console.WriteLine("4. Find Contact by Phone Number");
+                Console.WriteLine("5. Display All Contacts");
+                Console.WriteLine("6. Save Contacts to JSON");
+                Console.WriteLine("7. Save Contacts to XML");
+                Console.WriteLine("0. Exit");
+                Console.Write("Select an option: ");
+               
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Enter name: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Enter phone number: ");
+                        string phoneNumber = Console.ReadLine();
+                        manager.AddContact(new Contact { Name = name, PhoneNumber = phoneNumber });
+                        break;
+                    case "2":
+                        Console.Write("Enter name of the contact to remove: ");
+                        string nameToRemove = Console.ReadLine();
+                        manager.RemoveContact(nameToRemove);
+                        break;
+                    case "3":
+                        Console.Write("Enter name: ");
+                        string nameToFind = Console.ReadLine();
+                        var contactByName = manager.FindContactByName(nameToFind);
+                        if (contactByName != null)
+                        {
+                            Console.WriteLine($"Name: {contactByName.Name}, Phone Number: {contactByName.PhoneNumber}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Contact not found.");
+                        }
+                        break;
+                    case "4":
+                        Console.Write("Enter phone number: ");
+                        string phoneToFind = Console.ReadLine();
+                        var contactByPhone = manager.FindContactByPhoneNumber(phoneToFind);
+                        if (contactByPhone != null)
+                        {
+                            Console.WriteLine($"Name: {contactByPhone.Name}, Phone Number: {contactByPhone.PhoneNumber}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Contact not found.");
+                        }
+                        break;
+                    case "5":
+                        manager.DisplayContacts();
+                        break;
+                    case "6":
+                        Console.Write("Enter file path to save JSON: ");
+                        string jsonPath = Console.ReadLine();
+                        manager.SaveToJson(jsonPath);
+                        break;
+                    case "7":
+                        Console.Write("Enter file path to save XML: ");
+                        string xmlPath = Console.ReadLine();
+                        manager.SaveToXml(xmlPath);
+                        break;
+                    case "0":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option, please try again.");
+                        break;
+                   
+                }
+            }
         }
     }
 }
